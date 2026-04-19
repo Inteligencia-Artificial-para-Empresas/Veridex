157a68e62715fc2178f4aabe273e98a65e788fd1
Javier <javiergo.2502@gmail.com>
2026-04-19 16:46:19 -0500

    add: unindexable page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,20 @@
 // src/app/layout.tsx
 import QueryProvider from "@/providers/QueryProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Veridex",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
